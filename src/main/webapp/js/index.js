66264636e0b3608ee562c02802ad2004dfacc780
sgm01emilidarder <sgm01@iesemilidarder.com>
2020-02-16 13:11:24 +0100

    1

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,3 +1,25 @@
 function calculatePrice(price){
     $("#precio").val((price * $("#tiempoReserva").val() * $("#descuentoAplicado").val()).toFixed(1));
 }
+
+function checkUserAndPass(){
+    let pageContext = document.getElementById("pageContext").value;
+    let username = document.getElementById('user').value;
+    let password = document.getElementById('pass').value;
+    $.ajax({
+        type : "POST",
+        url : pageContext + "/client?action=login&userUsername=" + username +"&userPass=" + password,
+        timeout : 100000,
+        success : function() {
+            console.log("SUCCESS: ");
+            document.getElementById('loginForm').submit();
+        },
+        error : function(e) {
+            console.log("ERROR: ", e);
+            document.getElementById('alertMessage').className="alert alert-danger";
+        },
+        done : function(e) {
+            console.log("DONE");
+        }
+    });
+}
